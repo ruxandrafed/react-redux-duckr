68ebc796094fda73aee2d946254e79b8e245c57a
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-22 17:29:09 -0800

    Add more eslint rules and fix them

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,7 +4,7 @@ import { MainContainer } from '../containers'
 
 const routes = (
   <Router history={hashHistory}>
-    <Router path="/" component={MainContainer} />
+    <Router path='/' component={MainContainer} />
   </Router>
 )
 
