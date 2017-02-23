ee917bbd6c37dc5252bf193574383dd260616ae8
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-22 18:09:09 -0800

    Enable HMR for css modules

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { color } from './styles.css'
 
 const Main = React.createClass({
   render () {
     return (
-      <p>{'Hello World!'}</p>
+      <p className={color}>{'Hello World! Hello!'}</p>
     )
   },
 })
