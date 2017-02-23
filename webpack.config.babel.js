59ea4e054e3386fbf576a1b2beb108f302ad2990
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-22 18:34:38 -0800

    Add webpack resolve config that sets default path for app files so we can simplify import syntax

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -38,6 +38,9 @@ const base = {
       {test: /\.css$/, loader: 'style-loader!css-loader?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]'}
     ]
   },
+  resolve: {
+    modules: [path.resolve(__dirname, "app"), "node_modules"],
+  }
 }
 
 const developmentConfig = {
