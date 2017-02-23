384a6c5d48c3f5c97ee52f0f96b1ad26b184c4ff
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-22 17:22:19 -0800

    Add eslint, install plugins and config

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-const Main  = React.createClass({
-  render() {
-    return(
+const Main = React.createClass({
+  render () {
+    return (
       <p>Hello World!</p>
     )
   }
-});
+})
 
-export default Main;
+export default Main
