903b95a2715787339b48758e683b3c07cba74651
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-22 21:15:20 -0800

    Add navigation

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react'
+import { Navigation } from 'components'
 import { container, innerContainer } from './styles.css'
 
 const Main = React.createClass({
   render () {
     return (
       <div className={container}>
+        <Navigation isAuthed={true} />
         <div className={innerContainer}>
           {this.props.children}
         </div>
