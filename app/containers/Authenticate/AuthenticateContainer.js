7ab487100f1c174b379e584baef4d5d33bebb554
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-26 12:42:16 -0800

    Start refactoring handleAuth - have an action creator encapsulating/dispatching several a.c. over time

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react'
 import { Authenticate } from 'components'
-import auth from 'helpers/auth'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as userActionCreators from 'redux/modules/users'
@@ -9,17 +8,16 @@ const AuthenticateContainer = React.createClass({
   propTypes: {
     isFetching: PropTypes.bool.isRequired,
     error: PropTypes.string.isRequired,
-    authUser: PropTypes.func.isRequired,
-    fetchingUserSuccess: PropTypes.func.isRequired,
-    fetchingUserFailure: PropTypes.func.isRequired,
+    fetchAndHandleAuthedUser: PropTypes.func.isRequired,
   },
   handleAuth () {
-    this.props.fetchingUser()
-    auth().then((user) => {
-      this.props.fetchingUserSuccess(user.uid, user, Date.now())
-      this.props.authUser(user.uid)
-    })
-    .catch((error) => userActionCreators.fetchingUserFailure(error))
+    this.props.fetchAndHandleAuthedUser()
+//    this.props.fetchingUser()
+//    auth().then((user) => {
+//      this.props.fetchingUserSuccess(user.uid, user, Date.now())
+//      this.props.authUser(user.uid)
+//    })
+//    .catch((error) => userActionCreators.fetchingUserFailure(error))
   },
   render () {
     return (
