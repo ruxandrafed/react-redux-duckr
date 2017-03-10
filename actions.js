438e3e3f30230c4f30fcf6111b7221f63dfb0f6b
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-03-10 14:33:26 -0800

    Create ducks reducer

--- actions.js
+++ actions.js
@@ -1,33 +1,3 @@
-// Ducks
-{
-  type: FETCHING_DUCK,
-}
-
-{
-  type: FETCHING_DUCK_ERROR,
-  error: 'Error fetching Duck',
-}
-
-{
-  type: FETCHING_DUCK_SUCCESS,
-  duck,
-}
-
-{
-  type: REMOVE_FETCHING,
-}
-
-{
-  type: ADD_DUCK,
-  duck,
-}
-
-{
-  type: ADD_MULTIPLE_DUCKS,
-  ducks,
-}
-
-
 // Feed
 {
   type: SETTING_FEED_LISTENER,
