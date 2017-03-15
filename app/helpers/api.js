8b13b4c8549911afd617ce2814252c1b3df54236
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-03-14 18:16:03 -0700

    Connect feed to firebase

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -29,3 +29,13 @@ export function saveDuck (duck) {
     saveLikeCount(duckId),
   ]).then(() => ({...duck, duckId}))
 }
+
+export function listenToFeed (callback, errorCallback) {
+  ref.child('ducks').on('value', (snapshot) => {
+    const feed = snapshot.val() || {}
+    const sortedIds = Object.keys(feed).sort((a,b) => {
+      return feed[b].timestamp - feed[a].timestamp
+    })
+    callback({feed, sortedIds})
+  }, errorCallback)
+}
