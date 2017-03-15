a3b4c43ba4434f38192d0250fe53992903246e0c
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-03-14 18:17:25 -0700

    Build Feed UI

--- app/containers/Feed/FeedContainer.js
+++ app/containers/Feed/FeedContainer.js
@@ -6,6 +6,7 @@ import * as feedActionCreators from 'redux/modules/feed'
 
 const FeedContainer = React.createClass({
   propTypes: {
+    duckIds: PropTypes.array.isRequired,
     newDucksAvailable: PropTypes.bool.isRequired,
     error: PropTypes.string.isRequired,
     isFetching: PropTypes.bool.isRequired,
@@ -18,6 +19,7 @@ const FeedContainer = React.createClass({
   render () {
     return (
       <Feed
+        duckIds={this.props.duckIds}
         newDucksAvailable={this.props.newDucksAvailable}
         error={this.props.error}
         isFetching={this.props.isFetching}
@@ -28,11 +30,12 @@ const FeedContainer = React.createClass({
 })
 
 function mapStateToProps ({feed}) {
-  const { newDucksAvailable, error, isFetching } = feed;
+  const { newDucksAvailable, error, isFetching, duckIds } = feed;
   return {
     newDucksAvailable,
     error,
-    isFetching
+    isFetching,
+    duckIds,
   }
 }
 
